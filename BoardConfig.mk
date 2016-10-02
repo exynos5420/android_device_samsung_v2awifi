cd5c8e40d443e195bc254c88f62e1b4e9d21f54b
Joseph Annareddy <josephannareddy@gmail.com>
2016-10-02 06:04:33 -0700

    v2awifi: Remove IR Flag
    * Only the note family needs this flag, this is a tab
    
    Change-Id: I5e457f18a65be7778410c597d5e85cbebc04bf74
    Signed-off-by: Joseph Annareddy <josephannareddy@gmail.com>
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,9 +95,6 @@ BOARD_USES_TRUST_KEYMASTER := true
 # Include path
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
-# IR Blaster
-IR_HAS_ONE_FREQ_RANGE := true
-
 # Hardware
 BOARD_HARDWARE_CLASS += device/samsung/v2awifi/cmhw
 BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
