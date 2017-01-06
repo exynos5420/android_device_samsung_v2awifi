ad29d9f82dea66fc356f2dc37e02b8d905e12130
Thomas Patry <thompatry@gmail.com>
2017-01-05 20:32:08 -0600

    v2awifi:Fixes OK Google.
    I hate this damn bug. I would have never thought to look here.
    
    Basically changes the order of the inherit are called.
    
    Change-Id: I867f6190ed6361ab4d07906f04349d668cd40b3a

--- cm.mk
+++ cm.mk
@@ -1,8 +1,4 @@
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-
-# Inherit from v2awifi device
-$(call inherit-product, device/samsung/v2awifi/device.mk)
+$(call inherit-product, device/samsung/v2awifi/full_v2awifi.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
@@ -14,4 +10,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=SM-T900 \
     PRODUCT_NAME=v2awifi \
     PRODUCT_DEVICE=v2awifi \
-    TARGET_DEVICE=v2awifi
+    TARGET_DEVICE=v2awifi \
