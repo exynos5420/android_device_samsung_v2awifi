18ee2530b0417b3c951b5ed46092a72d8291b1c8
Vasyl Gello <vasek.gello@gmail.com>
2018-09-16 21:25:16 +0300

    v2awifi: Set build fingerprint from latest stock
    This allows passing SafetyNet profile check.
    
    Change-Id: I08b49431df1406cf745fc15af44a4121e636ef5a

--- lineage.mk
+++ lineage.mk
@@ -20,4 +20,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=SM-T900 \
     PRODUCT_NAME=v2awifi \
     PRODUCT_DEVICE=v2awifi \
+    PRIVATE_BUILD_DESC="samsung/v2wifixx/v2wifi:5.1.1/LMY47X/T900XXUBOH4:user/release-keys" \
     TARGET_DEVICE=v2awifi
+
+BUILD_FINGERPRINT := samsung/v2wifixx/v2wifi:5.1.1/LMY47X/T900XXUBOH4:user/release-keys
