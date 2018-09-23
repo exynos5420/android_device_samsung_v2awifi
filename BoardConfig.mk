37b21b5089c234176d88f39b9f8d42d1b1959940
Stricted <info@stricted.net>
2018-09-23 02:56:24 +0200

    v1awifi: set kernel defconfig
    Change-Id: I9749dbc662d55c12696bc70cfbd606ad392efb02

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,6 +22,9 @@ LOCAL_PATH := device/samsung/v2awifi
 # Assert
 TARGET_OTA_ASSERT_DEVICE := v2awifi
 
+# Kernel
+TARGET_KERNEL_CONFIG := lineageos_v2awifi_defconfig
+
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
