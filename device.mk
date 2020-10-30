ea40666b513a685d5beb607685e16f461afb7be1
Thomas Patry <thompatry@gmail.com>
2020-10-30 16:09:37 +0000

    v2awifi: Remove building stlport
    Change-Id: I448e2f9735cc8da2a041c78e5766418be691820e

--- device.mk
+++ device.mk
@@ -55,10 +55,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/sec_touchscreen.kl:system/usr/keylayout/sec_touchscreen.kl
 
-# libstlport
-PRODUCT_PACKAGES += \
-    libstlport
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
