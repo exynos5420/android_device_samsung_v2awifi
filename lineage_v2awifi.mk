d69d6faddd928161c6528675c1cb523cc4841470
Thomas Patry <thompatry@gmail.com>
2021-02-20 17:28:35 +0000

    Clean up old lineage.mk in support of the lineage_v2awifi.mk
    Change-Id: I2dd0854aeff40f228b639ef7ae22fc065c6e8ff5

--- lineage_v2awifi.mk
+++ lineage_v2awifi.mk
@@ -20,9 +20,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit from v2awifi device
 $(call inherit-product, device/samsung/v2awifi/device.mk)
 
+PRODUCT_NAME := full_v2awifi
+PRODUCT_DEVICE := v2awifi
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := SM-t900
+PRODUCT_MODEL := SM-T900
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
@@ -34,4 +36,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=SM-T900 \
     PRODUCT_NAME=v2awifi \
     PRODUCT_DEVICE=v2awifi \
+    PRIVATE_BUILD_DESC="samsung/v2wifixx/v2wifi:5.1.1/LMY47X/T900XXUBOH4:user/release-keys" \
     TARGET_DEVICE=v2awifi
+
+BUILD_FINGERPRINT := samsung/v2wifixx/v2wifi:5.1.1/LMY47X/T900XXUBOH4:user/release-keys
