fcuzzocrea/android_device_samsung_v1awifi

v1awifi: set kernel defconfig
Change-Id: I9749dbc662d55c12696bc70cfbd606ad392efb02

BoardConfig.mk
@@ -22,6 +22,9 @@ LOCAL_PATH := device/samsung/v1awifi
 # Assert
 TARGET_OTA_ASSERT_DEVICE := v1awifi
 
+# Kernel
+TARGET_KERNEL_CONFIG := lineageos_v1awifi_defconfig
+
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760

exynos5420/android_device_samsung_v2awifi

BoardConfig.mk
@@ -22,6 +22,9 @@ LOCAL_PATH := device/samsung/v2awifi
 # Assert
 TARGET_OTA_ASSERT_DEVICE := v2awifi
 
+# Kernel
+TARGET_KERNEL_CONFIG := lineageos_v2awifi_defconfig
+
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760