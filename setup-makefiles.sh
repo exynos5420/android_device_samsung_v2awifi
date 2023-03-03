b388306476714d285564bdbdd379cc78513d4b95
Thomas Patry <thompatry@gmail.com>
2023-03-03 00:50:27 +0000

    v2awifi: add universal5420-common to extract scripts
    Change-Id: Iecd6f7fe3f3ed15b933b12184db1d3ec57408771

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -10,6 +10,7 @@ set -e
 
 export DEVICE=v2awifi
 export DEVICE_COMMON=v2a-common
+export DEVICE_UNIVERSAL_COMMON=universal5420-common
 export VENDOR=samsung
 
 "./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
